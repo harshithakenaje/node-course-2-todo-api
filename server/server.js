34ce9f55c0a9eef007c8e990b2667e4ca70718b2
harshithakenaje <hks4evr@gmail.com>
2017-12-21 12:38:39 +0530

    GET todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectID}= require('mongodb');
 
 var {mongoose}= require('./db/mongoose');
 var {User} = require('./models/user');
@@ -19,24 +20,43 @@ var app = express();
 //     });
 // });
 
-app.use(bodyParser.json());
-app.post('/todos',(req,res)=>{
-    var todo= new Todo({
-        text: req.body.text
-    });
-    todo.save().then((rec)=>{
-        res.send(rec);
-    },(e)=>{
-        res.send(e);
-    });
+// app.use(bodyParser.json());
+// app.post('/todos',(req,res)=>{
+//     var todo= new Todo({
+//         text: req.body.text
+//     });
+//     todo.save().then((rec)=>{
+//         res.send(rec);
+//     },(e)=>{
+//         res.send(e);
+//     });
+// })
+// app.get('/todos',(req,res)=>{
+//     Todo.find().then((todos)=>{
+//         res.send({todos});
+//     },(err)=>{
+//         res.status(400).send(err);
+//     })
+// })
+
+app.get('/todos/:id',(req,res)=>{
+    var id= req.params.id;
+
+     if(!ObjectID.isValid(id)){
+   return res.status(404).send();
+ }
+Todo.findById(id).then((todo)=>{
+  if(!todo){
+   return res.status(404).send();
+  }
+  res.send({todo});
+ }).catch((e)=>{
+     res.status(400).send();
+ })
+
 })
-app.get('/todos',(req,res)=>{
-    Todo.find().then((todos)=>{
-        res.send({todos});
-    },(err)=>{
-        res.status(400).send(err);
-    })
-})
+
+ 
 
 app.listen(3000,()=>{
     console.log('Server started at port 3000:');
