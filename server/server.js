2320f291a80ff111e84b10c259d41466b4df9d30
harshithakenaje <hks4evr@gmail.com>
2017-12-28 12:07:43 +0530

    Add POST/user/login route

--- server/server.js
+++ server/server.js
@@ -121,6 +121,19 @@ app.get('/user/me',authenticate,(req,res)=>{
     res.send(req.user);
 });
 
+app.post('/user/login', (req,res)=>{
+  var body = _.pick(req.body, ['email', 'password']);
+
+  User.findByCredentials(body.email,body.password).then((user)=>{
+   return user.generateAuthToken()
+   .then((token)=>{
+    res.header('x-auth', token).send(user);
+   })
+  }).catch((e)=>{
+    res.status(400).send();
+  })
+});
+
 app.listen(port, () => {
   console.log(`Started up at port ${port}`);
 });
